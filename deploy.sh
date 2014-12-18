535aa8636ec3e34484aaa7cb0f8885ede36fef60
Michael Schaefer <michael.schaefer@uni-muenster.de>
2014-12-18 09:57:31 +0100

    rsync via checksum, .htaccess for forwarding to https

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
 
-rsync -rtv --progress -s /home/mscha_08/development/michael-schaefer.org/_site/ ssh-w005d352@w005d352.kasserver.com:/blog/
+cd _site
+rsync -acv . ssh-w005d352@w005d352.kasserver.com:/blog/
+cd ..
